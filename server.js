167937c6f934cd66660d051e4e476e7420deec2f
MajestroM94 <123757131+MajestroM94@users.noreply.github.com>
2024-11-25 00:57:20 +0300

    Fix server.js file

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const contactsRouter = require('./routes/contacts');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-// Подключение к MongoDB
-mongoose.connect('mongodb+srv://maxqwerty177:8UrFzLcXUHWds7Vh@cluster0.hqdp9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+// Подключение к MongoDB Atlas
+mongoose.connect('mongodb+srv://maxqwerty177:8UrFzLcXUHWds7Vh@cluster0.hqdp9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Middleware
 app.use(bodyParser.json());
